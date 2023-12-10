9b3249072b5e8abade7d68af7ff9539f48e79a6e
Youngboyvungtau <23520228@gm.uit.edu.vn>
2023-12-10 20:19:16 +0700

    dang
    dang

--- 23520228_BT05/Bai233/BAi233.cpp
+++ 23520228_BT05/Bai233/BAi233.cpp
@@ -25,10 +25,13 @@ void LietKe(PHANSO[][100], int, int);
 int main()
 {
 	PHANSO ln[100][100];
+	PhanSo ps;
 	int i, j;
 	Nhap(ln, i, j);
-	cout << "\nPhan so vua nhap: \n";
 	Xuat(ln, i, j);
+	Nhap(ps);
+	Xuat(ps);
+	LietKe(ln, i, j);
 	return 1;
 }
 
@@ -70,4 +73,68 @@ void Xuat(PHANSO a[][100], int m, int n)
 			Xuat(a[i][j]);
 			cout << endl;
 		}
+}
+
+int UCLN(int a, int b)
+{
+	a = abs(a);
+	b = abs(b);
+	while (a * b != 0)
+		if (a > b)
+			a = a - b;
+		else
+			b = b - a;
+	return a + b;
+}
+
+int ktToiGian(PHANSO x)
+{
+	if (UCLN(x.Tu, x.Mau) == 1)
+		return 1;
+	return 0;
+}
+
+void HoanVi(PHANSO& x, PHANSO& y)
+{
+	PHANSO temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+int SoSanh(PHANSO x, PHANSO y)
+{
+	float a = (float)x.Tu / x.Mau;
+	float b = (float)x.Mau / x.Tu;
+	if (a > b)
+		return 1;
+	if (a < b)
+		return -1;
+	return 0;
+}
+
+void SapTang(PHANSO a[], int n)
+{
+	for (int i = 0; i <= n - 2; i++)
+		for (int j = i + 1; j <= n - 1; j++)
+			if (SoSanh(a[i], a[j]) == 1)
+				HoanVi(a[i], a[j]);
+}
+
+void Xuat(PHANSO a[], int n)
+{
+	for (int i = 0; i <= n - 1; i++)
+		Xuat(a[i]);
+}
+
+void LietKe(PHANSO a[][100], int m, int n)
+{
+	PHANSO b[1000];
+	int k = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (ktToiGian(a[i][j]))
+				b[k++] = a[i][j];
+	SapTang(b, k);
+	Xuat(b, k);
 }
